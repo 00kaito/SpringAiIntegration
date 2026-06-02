7a59028cc287db7a533a9697e59610eb1e36ca13
hpz4 <sham.bednarz@gmail.com>
2026-06-02 10:52:25 -0400

    init

---
+++ src/main/java/com/haddad/springaiintegration/SpringAiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.haddad.springaiintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haddad/springaiintegration/SpringAiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haddad.springaiintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
